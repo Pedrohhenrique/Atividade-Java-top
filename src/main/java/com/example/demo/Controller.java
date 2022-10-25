85ffccab844c7935b6b2e4ffe773de61c2714948
Pedrohhenrique <pedrohdosantosss@gmail.com>
2022-10-25 19:53:48 -0300

    Aula

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import ch.qos.logback.core.net.server.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/cliente/v1")
+
+public class Controller {
+
+    @Autowired
+    Repository resposity;
+
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente)
+    {
+        Cliente clienteSaved = resposity.save(cliente);
+        return clienteSaved;
+    }
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Optional<Cliente> getClienteById(@PathVariable Long id){
+        Optional<Cliente> clienteReturned = resposity.findById(id);
+        return clienteReturned;
+    }
+    @DeleteMapping("/id")
+    public void  deleteCliente(@PathVariable Long id){
+        resposity.deleteById(id);
+    }
+}
